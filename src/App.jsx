03308b7422deafedf7a34a8b0088f23bd94e6404
sumitbariya807-jpg <sumitbariya807@gamil.com>
2026-03-13 15:52:00 +0530

    Fix GitHub Pages routing: BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom'
 import './App.css'
 import { sendConfirmationEmail, sendStatusUpdateEmail } from './useEmail.js'
 
